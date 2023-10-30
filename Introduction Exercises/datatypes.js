4c33b6ba368751321723410ee3d9239386e3b0d3
ayocesarallie37 <2202002@utrivieramaya.edu.mx>
2023-10-30 15:32:23 -0600

    first commit

---
+++ Introduction Exercises/datatypes.js
@@ -0,0 +1,46 @@
+// Para ejecutar cada ejercicio, elimina "/*" y "*/" para poder revisar el ejercicio. 
+
+/*
+console.log(typeof 'César')
+console.log(typeof 18)
+console.log(typeof null)
+console.log(typeof undefined)
+*/
+
+// Declarar 4 variables sin asignar valores.
+// Área del trapecio
+/*
+let BaseMayor
+let BaseMenor
+let altura
+let resul
+*/
+
+// Declarar variables asignando valores.
+// Datos personales
+/*
+let name = 'Daniel'
+let lastName = 'Pérez'
+let age = '19'
+let weight = '80'
+*/
+
+// Declarar variables asignando mi nombre.
+/*
+let nombre = 'César'
+let apellido = 'Pérez'
+let EstadoCivil = 'Soltero'
+let país = 'México'
+let edad = 20
+*/
+
+// Declarar las variables anteriores en una sola línea.
+// let nombre = 'César', apellido = 'Pérez', EstadoCivil = 'Soltero', país = 'México', edad = 20
+
+// Declara variables "myAge" y "your Age" y asignar valores iniciales.
+
+let myAge = 20
+let yourAge = 19
+
+console.log('Mi edad es: '+ myAge)
+console.log('Tu edad es: '+yourAge)
